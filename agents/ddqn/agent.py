66379a4df97458fa7576642c2509c9599599b165
martinhoT <martinho.tavarex@gmail.com>
2023-10-16 21:26:02 +0100

    Added untested 'ddqn' implementation

---
+++ agents/ddqn/agent.py
@@ -0,0 +1,146 @@
+from agents.base import FootsiesAgentBase
+from typing import Any, Iterator
+import numpy as np
+import torch
+from torch import nn
+from gymnasium.spaces import Space
+from gymnasium.spaces.utils import flatten_space
+from collections import namedtuple, deque
+
+
+class QNetwork(nn.Module):
+    def __init__(self, input_size: int):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, 16), nn.Tanh(), nn.Linear(16, 3)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.layers(x)
+        return logits
+
+
+Transition = namedtuple("Transition", ["obs", "action", "next_obs", "reward"])
+
+
+class ReplayMemory:
+    def __init__(self, capacity: int):
+        self.deque = deque([], maxlen=capacity)
+
+    def add_transition(self, transition: Transition):
+        self.deque.append(transition)
+
+    def __iter__(self) -> Iterator[Transition]:
+        return self.deque.__iter__()
+
+    def random_sample(self) -> Iterator[Transition]:
+        for transition in self.deque:
+            if np.random.random() < 0.5:
+                yield transition
+
+
+class FootsiesAgent(FootsiesAgentBase):
+    def __init__(
+        self,
+        observation_space: Space,
+        action_space: Space,
+        replay_memory_capacity: int = 100,
+        update_frequency: int = 30,
+        learning_rate: float = 0.1,
+        discount_factor: float = 0.9,
+        epsilon: float = 0.9,
+        epsilon_decay_rate: float = 0.001,
+        min_epsilon: float = 0.05,
+        target_update_rate: float = 0.01,
+        device: torch.device = "cpu",
+    ):
+        self.update_frequency = update_frequency
+        self.epsilon = epsilon
+        self.epsilon_decay_rate = epsilon_decay_rate
+        self.min_epsilon = min_epsilon
+        self.target_update_rate = target_update_rate
+        self.learning_rate = learning_rate
+        self.action_space = action_space
+
+        # The networks are technically not Q-networks, since they don't define Q-values for action combinations
+        flattened_observation_size = flatten_space(observation_space).shape[0]
+        self.policy_network = QNetwork(flattened_observation_size).to(device)
+        self.target_network = QNetwork(flattened_observation_size).to(device)
+        self.replay_memory = ReplayMemory(replay_memory_capacity)
+
+        self.optimizer = torch.optim.RMSprop(
+            self.policy_network.parameters(), lr=learning_rate
+        )
+        self.loss_function = lambda output, target, reward: min(
+            1, max(-1, reward + discount_factor * target - output)
+        )  # clipped
+
+        self.current_iteration = 0
+        self.current_observation = None
+        self.current_action = None
+
+    def act(self, obs) -> Any:
+        self.current_iteration = obs
+        action = self.policy(obs)
+        self.current_action = action
+        return action
+
+    def policy(self, obs) -> Any:
+        # Exploration
+        if np.random.random() < self.epsilon:
+            return self.action_space.sample()
+
+        # Greedy
+        with torch.no_grad():  # don't calculate gradients
+            logits = self.policy_network(obs)
+        return nn.Tanh()(logits) > 0.0
+
+    def update(self, next_obs, reward: float):
+        transition = Transition(
+            self.current_observation, self.current_action, next_obs, reward
+        )
+        self.replay_memory.add_transition(transition)
+
+        # Update networks
+        if self.current_iteration == self.update_frequency - 1:
+            self.optimizer.zero_grad()  # prevent double-counting
+
+            for (
+                t_obs,
+                t_action,
+                t_next_obs,
+                t_reward,
+            ) in self.replay_memory.random_sample():
+                output_logits = self.policy_network(t_obs)
+                target_logits = self.target_network(t_next_obs)
+
+                # Because actions are combinations of the final network output, we need to this a bit differently.
+                # Due to this, the greedy action selection is defined differently
+                output = output_logits[t_action].sum()
+                target = target_logits[
+                    target_logits > 0.0
+                ].sum()  # since it's a combination of actions
+
+                loss = self.loss_function(output, target, t_reward)
+                loss.backward()
+
+            self.optimizer.step()
+
+            self.target_network.load_state_dict(
+                {
+                    k: v_policy * self.target_update_rate
+                    + v_target * (1 - self.target_update_rate)
+                    for k, v_policy, _, v_target in zip(
+                        self.target_network.state_dict(),
+                        self.policy_network.state_dict(),
+                    )
+                }
+            )
+
+            self.epsilon = self.epsilon + self.epsilon_decay_rate * (
+                self.min_epsilon - self.epsilon
+            )
+
+        self.current_iteration = (self.current_iteration + 1) % self.update_frequency
