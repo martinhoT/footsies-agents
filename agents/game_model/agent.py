Opponent model further experimentation (scaling, mirroring fixes, last n actions)

agents/game_model/agent.py
@@ -54,7 +54,7 @@ class FootsiesAgent(FootsiesAgentBase):
         action_space: Space,
         by_primitive_actions: bool = False,
         move_transition_scale: float = 10.0, # scale training examples where move transitions occur, since they are very important
-        optimize_frequency: int = 1000, # mini-batch size, bad name
+        mini_batch_size: int = 1000,
         learning_rate: float = 1e-2,
         hidden_layer_sizes_specification: str = "64,64",
         hidden_layer_activation_specification: str = "LeakyReLU",
@@ -67,7 +67,7 @@ class FootsiesAgent(FootsiesAgentBase):
         self.agent_action_dim = action_space.shape[0] if by_primitive_actions else ActionMap.n_simple()
         self.opponent_action_dim = self.agent_action_dim   # we assume they use the same action space
         self.move_transition_scale = move_transition_scale
-        self.optimize_frequency = optimize_frequency
+        self.mini_batch_size = mini_batch_size
         self.learning_rate = learning_rate
 
         self.game_model = GameModel(
@@ -95,7 +95,7 @@ class FootsiesAgent(FootsiesAgentBase):
     def _action_to_tensor(self, action: int, num_classes: int) -> torch.Tensor:
         return nn.functional.one_hot(torch.tensor(action), num_classes=num_classes).unsqueeze(0)
 
-    def _update_batch(self, obs: np.ndarray, agent_action: int, opponent_action: int, next_obs):
+    def _update_batch(self, obs: np.ndarray, agent_action: int, opponent_action: int, next_obs: np.ndarray):
         obs = self._obs_to_tensor(obs)
         next_obs = self._obs_to_tensor(next_obs)
         agent_action_oh = self._action_to_tensor(agent_action, self.agent_action_dim)
@@ -120,7 +120,7 @@ class FootsiesAgent(FootsiesAgentBase):
 
         self._update_batch(self.current_observation, agent_action, opponent_action, next_obs)
 
-        if len(self.state_batch_as_list) >= self.optimize_frequency:
+        if len(self.state_batch_as_list) >= self.mini_batch_size:
             batch = torch.cat(self.state_batch_as_list)
             loss = self.train(batch)
             
@@ -140,8 +140,8 @@ class FootsiesAgent(FootsiesAgentBase):
         # Euclidean distance
         guard_distance = torch.sqrt(torch.sum((predicted[:, 0:2] - batch_y[:, 0:2])**2, dim=1))
         # Cross entropy loss
-        move_distance_p1 = -torch.sum(torch.log(predicted[:, 2:17]) * batch_y[:, 2:17], dim=1)
-        move_distance_p2 = -torch.sum(torch.log(predicted[:, 17:32]) * batch_y[:, 17:32], dim=1)
+        move_distance_p1 = -torch.sum(torch.log2(predicted[:, 2:17]) * batch_y[:, 2:17], dim=1)
+        move_distance_p2 = -torch.sum(torch.log2(predicted[:, 17:32]) * batch_y[:, 17:32], dim=1)
         # Euclidean distance
         move_progress_and_position_distance = torch.sqrt(torch.sum((predicted[:, 32:36] - batch_y[:, 32:36])**2, dim=1))
 

agents/mimic/agent.py
@@ -1,3 +1,4 @@
+from collections import deque
 from copy import deepcopy
 import os
 import numpy as np
@@ -7,7 +8,7 @@ from agents.base import FootsiesAgentBase
 from agents.torch_utils import create_layered_network, InputClip, DebugStoreRecent
 from agents.action import ActionMap
 from gymnasium import Env
-from typing import Callable, List, Tuple
+from typing import Callable, Iterable, List, Tuple
 from gymnasium import Space
 from footsies_gym.moves import FootsiesMove, FOOTSIES_MOVE_INDEX_TO_MOVE
 
@@ -91,7 +92,7 @@ class PlayerModel:
             self.loss_function = lambda predicted, target: torch.mean((predicted - target)**2, dim=1)
         # Cross entropy loss
         else:
-            self.loss_function = lambda predicted, target: -torch.sum(torch.log(predicted) * target, dim=1)
+            self.loss_function = lambda predicted, target: -torch.sum(torch.log2(predicted) * target, dim=1)
 
         self.optimizer = torch.optim.SGD(params=self.network.parameters(), lr=learning_rate)
 
@@ -115,10 +116,8 @@ class PlayerModel:
 
         self.x_batch_as_list = []
         self.y_batch_as_list = []
+        self.move_transition_as_list = []
         self.step = 0
-        # Whether the first observation in the training batch corresponds to a move transition in the part of the player.
-        # Important when determining the move transtion scale for each observation
-        self.first_obs_of_batch_had_move_transition = None
         self.cummulative_loss = 0
         self.cummulative_loss_n = 0
 
@@ -128,22 +127,18 @@ class PlayerModel:
     def update(self, obs: torch.Tensor, action: torch.Tensor, move_transition: bool = False):
         self.x_batch_as_list.append(obs)
         self.y_batch_as_list.append(action)
+        self.move_transition_as_list.append(move_transition)
         self.step += 1
-        if self.first_obs_of_batch_had_move_transition is None:
-            self.first_obs_of_batch_had_move_transition = move_transition
 
         if self.step >= self.optimize_frequency:
             batch_x = torch.cat(self.x_batch_as_list, 0)
             batch_y = torch.cat(self.y_batch_as_list, 0)
             loss = None
-            base_lr = self.optimizer.param_groups[0]["lr"]
-            lr_modifier = 1
 
-            move_transition_multiplier = 1 + torch.hstack((torch.tensor(self.first_obs_of_batch_had_move_transition), torch.any(batch_x[:-1, 2:32] != batch_x[1:, 2:32], dim=1))) * (self.move_transition_scale - 1)
+            move_transition_multiplier = 1 + torch.tensor(self.move_transition_as_list) * (self.move_transition_scale - 1)
 
             # Keep training on examples that are problematic
             while loss is None or loss > self.max_loss:
-                self.optimizer.param_groups[0]["lr"] = base_lr * lr_modifier
                 self.optimizer.zero_grad()
 
                 predicted = self.network(batch_x)
@@ -165,7 +160,6 @@ class PlayerModel:
                         f"learning is dead, gradients are 0! (loss: {loss.item()})"
                     )
 
-            self.optimizer.param_groups[0]["lr"] = base_lr
             self.x_batch_as_list.clear()
             self.y_batch_as_list.clear()
             self.step = 0
@@ -241,7 +235,8 @@ class FootsiesAgent(FootsiesAgentBase):
         observation_space: Space,
         action_space: Space,
         frameskipping: bool = True,
-        append_time_since_last_interaction: bool = False,
+        append_last_actions_n: int = 0,
+        append_last_actions_distinct: bool = True,
         by_primitive_actions: bool = False,
         use_sigmoid_output: bool = False,
         input_clip: bool = False,
@@ -255,19 +250,23 @@ class FootsiesAgent(FootsiesAgentBase):
         learn_p1: bool = True,
         learn_p2: bool = True,
     ):
-        if append_time_since_last_interaction:
-            raise NotImplementedError("appending the time since the last interaction is still not supported")
-
+        if by_primitive_actions:
+            raise NotImplementedError("imitating primitive actions is not supported yet")
+        
         self.frameskipping = frameskipping
+        self.append_last_actions_n = append_last_actions_n
+        self.append_last_actions_distinct = append_last_actions_distinct
         self.by_primitive_actions = by_primitive_actions
         self.learn_p1 = learn_p1
         self.learn_p2 = learn_p2
 
+        # This full observation size doesn't consider primitive actions yet
+        full_observation_size = observation_space.shape[0] + append_last_actions_n * ActionMap.n_simple()
         self.n_moves = action_space.n if by_primitive_actions else ActionMap.n_simple()
         
         # Masks to selectively remove the portion of the observation that is not relevant (occurs if frameskipping, it is the player's move progress)
-        p1_observation_mask = torch.ones((observation_space.shape[0],), dtype=torch.bool)
-        p2_observation_mask = torch.ones((observation_space.shape[0],), dtype=torch.bool)
+        p1_observation_mask = torch.ones((full_observation_size,), dtype=torch.bool)
+        p2_observation_mask = torch.ones((full_observation_size,), dtype=torch.bool)
         if self.frameskipping:
             p1_observation_mask[32] = False
             p2_observation_mask[33] = False
@@ -275,7 +274,7 @@ class FootsiesAgent(FootsiesAgentBase):
         # Both models have mostly the same structure and training regime
         player_model_kwargs = {
             # We don't consider the move progress of the player themselves (e.g. if I'm modeling player 2, then I don't need to know its move progress)
-            "obs_size": observation_space.shape[0] - (1 if self.frameskipping else 0),
+            "obs_size": full_observation_size - (1 if self.frameskipping else 0),
             "n_moves": self.n_moves,
             "mini_batch_size": mini_batch_size,
             "use_sigmoid_output": use_sigmoid_output,
@@ -299,6 +298,11 @@ class FootsiesAgent(FootsiesAgentBase):
         self.previous_observation = None
         self.previous_p1_move_state: FootsiesMove = None
         self.previous_p2_move_state: FootsiesMove = None
+        self.previous_p1_move: int = None
+        self.previous_p2_move: int = None
+        # Fill the action history with STANDs
+        self.p1_action_history: deque[int] = deque([0] * self.append_last_actions_n, maxlen=self.append_last_actions_n)
+        self.p2_action_history: deque[int] = deque([0] * self.append_last_actions_n, maxlen=self.append_last_actions_n)
 
         self._test_observations = None
         self._test_actions = None
@@ -314,6 +318,8 @@ class FootsiesAgent(FootsiesAgentBase):
         self.previous_observation = obs
         self.previous_p1_move_state = FOOTSIES_MOVE_INDEX_TO_MOVE[info["p1_move"]]
         self.previous_p2_move_state = FOOTSIES_MOVE_INDEX_TO_MOVE[info["p2_move"]]
+        self.previous_p1_move = ActionMap.simple_from_move_index(info["p1_move"])
+        self.previous_p2_move = ActionMap.simple_from_move_index(info["p2_move"])
 
         if predict:
             model = self.p1_model if p1 else self.p2_model
@@ -332,16 +338,19 @@ class FootsiesAgent(FootsiesAgentBase):
         p2_move_state: FootsiesMove = FOOTSIES_MOVE_INDEX_TO_MOVE[info["p2_" + key]]
         p1_simple = ActionMap.simple_from_move(p1_move_state)
         p2_simple = ActionMap.simple_from_move(p2_move_state)
-        p1_simple_oh = self._move_onehot(p1_simple)
-        p2_simple_oh = self._move_onehot(p2_simple)
+        p1_simple_oh = self._action_onehot(p1_simple)
+        p2_simple_oh = self._action_onehot(p2_simple)
 
-        p1_observation = self.p1_model.mask_environment_observation(self.previous_observation)
-        p2_observation = self.p2_model.mask_environment_observation(self.previous_observation)
+        p1_observation = self.craft_observation(self.previous_observation, True, True)
+        p2_observation = self.craft_observation(self.previous_observation, False, False)
 
         if self.learn_p1 and (not self.frameskipping or ActionMap.is_state_actionable_late(self.previous_p1_move_state, self.previous_observation[0, 32], next_obs[32])):
-            self.p1_model.update(p1_observation, p1_simple_oh, self.previous_p1_move_state != p1_move_state)
+            self.p1_model.update(p1_observation, p1_simple_oh, self.previous_p1_move != p1_simple and ActionMap.is_simple_action_commital(p1_simple))
         if self.learn_p2 and (not self.frameskipping or ActionMap.is_state_actionable_late(self.previous_p2_move_state, self.previous_observation[0, 33], next_obs[33])):
-            self.p2_model.update(p2_observation, p2_simple_oh, self.previous_p2_move_state != p2_move_state)
+            self.p2_model.update(p2_observation, p2_simple_oh, self.previous_p2_move != p2_simple and ActionMap.is_simple_action_commital(p2_simple))
+
+        self.append_to_action_history(p1_simple, True)
+        self.append_to_action_history(p2_simple, False)
 
         # Update metrics
         # self._p1_correct = (self._p1_correct * self._correct_decay) + (
@@ -357,8 +366,25 @@ class FootsiesAgent(FootsiesAgentBase):
     def _obs_to_tensor(self, obs):
         return torch.tensor(obs, dtype=torch.float32).reshape((1, -1))
 
-    def _move_onehot(self, move: int):
-        return nn.functional.one_hot(torch.tensor(move), num_classes=self.n_moves).unsqueeze(0)
+    def _action_onehot(self, action: int):
+        return nn.functional.one_hot(torch.tensor(action), num_classes=self.n_moves).unsqueeze(0)
+
+    def append_to_action_history(self, action: int, p1: bool):
+        if not self.append_last_actions_n:
+            return
+        
+        action_history = self.p1_action_history if p1 else self.p2_action_history
+
+        if not (self.append_last_actions_distinct and action_history[-1] == action):
+            action_history.append(action)
+
+    def craft_observation(self, obs: torch.Tensor, use_p1_model: bool, use_p1_action_history: bool) -> torch.Tensor:
+        model = self.p1_model if use_p1_model else self.p2_model
+        action_history = self.p1_action_history if use_p1_action_history else self.p2_action_history
+
+        full_observation = torch.hstack((obs,) + tuple(self._action_onehot(action) for action in action_history))
+        
+        return model.mask_environment_observation(full_observation)
 
     def decision_entropy(self, obs: torch.Tensor, p1: bool) -> float:
         model = self.p1_model if p1 else self.p2_model

analyse_opponent_model.py
@@ -1,10 +1,11 @@
 import dearpygui.dearpygui as dpg
 import numpy as np
-import torch
 from gymnasium.wrappers.flatten_observation import FlattenObservation
 from footsies_gym.envs.footsies import FootsiesEnv
 from footsies_gym.wrappers.action_comb_disc import FootsiesActionCombinationsDiscretized
 from footsies_gym.wrappers.normalization import FootsiesNormalized
+from footsies_gym.moves import FootsiesMove
+from agents.torch_utils import observation_invert_perspective_flattened
 from analysis import Analyser
 from agents.mimic.agent import FootsiesAgent as OpponentModelAgent
 from agents.action import ActionMap
@@ -77,7 +78,9 @@ class OpponentDistributionPlot:
 
 
 class MimicAnalyserManager:
-    def __init__(self):
+    def __init__(self, p1_mirror_p2: bool = False):
+        self.p1_mirror_p2 = p1_mirror_p2
+
         self.p1_plot = OpponentDistributionPlot("Player 1 move probability distribution")
         self.p2_plot = OpponentDistributionPlot("Player 2 move probability distribution")
 
@@ -106,6 +109,9 @@ class MimicAnalyserManager:
     def toggle_p2_online_learning(self):
         AGENT.learn_p2 = not AGENT.learn_p2
 
+    def toggle_p1_mirror_p2(self):
+        self.p1_mirror_p2 = not self.p1_mirror_p2
+
     def update_max_loss(self, max_loss: float):
         AGENT.p1_model.max_loss = max_loss
         AGENT.p2_model.max_loss = max_loss
@@ -114,6 +120,7 @@ class MimicAnalyserManager:
         with dpg.group(horizontal=True):
             dpg.add_checkbox(label="Enable P1 online learning", default_value=AGENT.learn_p1, callback=self.toggle_p1_online_learning)
             dpg.add_checkbox(label="Enable P2 online learning", default_value=AGENT.learn_p2, callback=self.toggle_p2_online_learning)
+            dpg.add_checkbox(label="P1 mirror P2", default_value=self.p1_mirror_p2, callback=self.toggle_p1_mirror_p2)
             dpg.add_input_float(label="Max. allowed loss", default_value=AGENT.p1_model.max_loss, width=100, callback=lambda s, a: self.update_max_loss(a))
             self.p1_frameskip = dpg.add_checkbox(label="P1 frameskip", default_value=False, enabled=False)
             self.p2_frameskip = dpg.add_checkbox(label="P2 frameskip", default_value=False, enabled=False)
@@ -168,11 +175,19 @@ class MimicAnalyserManager:
             # We need to call act so that the agent can store the current observation. Implementation detail, but whatever
             AGENT.act(analyser.previous_observation, analyser.previous_info)
             AGENT.update(analyser.current_observation, None, None, None, analyser.current_info)
+        # Update the action histories even when not performing online learning
+        else:
+            AGENT.append_to_action_history(p1_simple, True)
+            AGENT.append_to_action_history(p2_simple, False)
 
         observation_torch = AGENT._obs_to_tensor(observation)
-        p1_distribution_predicted = AGENT.p1_model.probability_distribution(AGENT.p1_model.mask_environment_observation(observation_torch)).squeeze()
-        p2_distribution_predicted = AGENT.p2_model.probability_distribution(AGENT.p2_model.mask_environment_observation(observation_torch)).squeeze()
-        p1_distribution_estimated = self.p1_action_table.probability_distribution(observation)
+        if self.p1_mirror_p2:
+            inverted = observation_invert_perspective_flattened(observation_torch)
+            p1_distribution_predicted = AGENT.p2_model.probability_distribution(AGENT.craft_observation(inverted, False, True)).squeeze()
+        else:
+            p1_distribution_predicted = AGENT.p1_model.probability_distribution(AGENT.craft_observation(observation_torch, True, True)).squeeze()
+        p1_distribution_estimated = self.p1_action_table.probability_distribution(observation) # we don't mirror this one, it's not that important
+        p2_distribution_predicted = AGENT.p2_model.probability_distribution(AGENT.craft_observation(observation_torch, False, False)).squeeze()
         p2_distribution_estimated = self.p2_action_table.probability_distribution(observation)
 
         self.p1_plot.update(p1_distribution_predicted, p1_distribution_estimated)
@@ -182,28 +197,21 @@ class MimicAnalyserManager:
         dpg.set_value(self.p2_action_table_estimator_size, str(self.p2_action_table.size()))
 
     def p2_prediction_discrete(self, obs: np.ndarray, info: dict) -> int:
+        if not self.p1_mirror_p2:
+            return 0
+        
         try:
             discrete_action = next(self.p2_predicted_action_iterator)
         except StopIteration:
             obs = AGENT._obs_to_tensor(obs)
             
-            # We need to craft the observation to be as if P1 is the one experiencing it
-            #  guard
-            obs[:, [0, 1]] = obs[:, [1, 0]]
-            #  move
-            tmp = obs[:, 2:17].clone().detach()
-            obs[:, 2:17] = obs[:, 17:32]
-            obs[:, 17:32] = tmp
-            #  move progress
-            obs[:, [32, 33]] = obs[:, [33, 32]]
-            #  position
-            obs[:, [34, 35]] = obs[:, [35, 34]]
+            # We need to craft the observation to be as if P1 is the one experiencing it (since P2 model was trained inverted)
+            obs = observation_invert_perspective_flattened(obs)
             
-            simple_action = AGENT.p2_model.predict(AGENT.p2_model.mask_environment_observation(obs), deterministic=True).item()
-            print(simple_action)
+            simple_action = AGENT.p2_model.predict(AGENT.craft_observation(obs, use_p1_model=False, use_p1_action_history=True), deterministic=True).item()
             self.p2_predicted_action_iterator = iter(ActionMap.simple_to_discrete(simple_action))
             discrete_action = next(self.p2_predicted_action_iterator)
-        
+
         return discrete_action
 
 
@@ -230,25 +238,38 @@ if __name__ == "__main__":
         observation_space=env.observation_space,
         action_space=env.action_space,
         frameskipping=True,
+        append_last_actions_n=60,
+        append_last_actions_distinct=False,
         by_primitive_actions=False,
         use_sigmoid_output=False,
-        input_clip=False,
+        input_clip=True,
         input_clip_leaky_coef=0.01,
         max_allowed_loss=float("+inf"),
-        hidden_layer_sizes_specification="",
-        hidden_layer_activation_specification="Identity",
+        hidden_layer_sizes_specification="128",
+        hidden_layer_activation_specification="LeakyReLU",
         mini_batch_size=1,
         learning_rate=0.005,
-        move_transition_scale=30,
+        move_transition_scale=100,
     )
 
     # load_agent_model(AGENT, "mimic_linear_frameskip")
 
     mimic_analyser_manager = MimicAnalyserManager()
 
+    def spammer():
+        from itertools import cycle
+        for action in cycle((
+            ActionMap.simple_to_discrete(ActionMap.SIMPLE_ACTIONS.index(FootsiesMove.N_ATTACK))[0],
+            ActionMap.simple_to_discrete(ActionMap.SIMPLE_ACTIONS.index(FootsiesMove.STAND))[0]
+        )):
+            yield action
+
+    spamming = spammer()
+
     analyser = Analyser(
         env=env,
-        p1_action_source=mimic_analyser_manager.p2_prediction_discrete,
+        # p1_action_source=mimic_analyser_manager.p2_prediction_discrete,
+        p1_action_source=lambda o, i: next(spamming),
         custom_elements_callback=mimic_analyser_manager.include_mimic_dpg_elements,
         custom_state_update_callback=mimic_analyser_manager.predict_next_move,
     )