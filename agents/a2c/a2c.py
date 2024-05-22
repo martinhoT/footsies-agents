Ability to not use softmax distributions (opp and policy), fixed `the_one` opponent extraction with action masking

agents/a2c/a2c.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 import logging
 from torch import nn
 from torch.distributions import Categorical
-from agents.torch_utils import create_layered_network, ToMatrix
+from agents.torch_utils import create_layered_network, ToMatrix, ProbabilityDistribution
 from agents.ql.ql import QFunction
 from abc import ABC, abstractmethod
 from enum import Enum
@@ -47,6 +47,7 @@ class ActorNetwork(nn.Module):
         opponent_action_dim: int | None = None,
         footsies_masking: bool = True,
         p1: bool = True,
+        use_softmax: bool = True,
     ):
         super().__init__()
         if opponent_action_dim is None:
@@ -69,7 +70,7 @@ class ActorNetwork(nn.Module):
         self.actor_layers = create_layered_network(obs_dim, output_dim, hidden_layer_sizes, hidden_layer_activation)
         if consider_opponent_action:
             self.actor_layers.append(ToMatrix(opponent_action_dim, action_dim))
-        self.softmax = nn.Softmax(dim=-1)
+        self.prob = nn.Softmax(dim=-1) if use_softmax else ProbabilityDistribution(dim=-1)
 
         # What actions can be performed during hitstop
         self._hitstop_mask = T.zeros((1, opponent_action_dim, action_dim), dtype=T.bool)
@@ -93,7 +94,7 @@ class ActorNetwork(nn.Module):
         if action_mask is not None:
             # Invalidate all actions that are not in the mask
             logits = logits.masked_fill(~action_mask, -T.inf)
-        return self.softmax(logits / temperature)
+        return self.prob(logits / temperature)
     
     @property
     def consider_opponent_action(self) -> bool:
@@ -156,6 +157,15 @@ class ActorNetwork(nn.Module):
         cloned.load_state_dict(self.state_dict())
 
         return cloned
+
+    @property
+    def p1(self) -> bool:
+        """Whether this policy is for player 1."""
+        return self._p1
+
+    @p1.setter
+    def p1(self, value: bool):
+        self._p1 = value
 
 
 class A2CLearnerBase(ABC):

agents/mimic/mimic.py
@@ -1,7 +1,7 @@
 import torch as T
 import logging
 from torch import nn
-from agents.torch_utils import create_layered_network, InputClip, DebugStoreRecent
+from agents.torch_utils import create_layered_network, InputClip, ProbabilityDistribution, LogProbabilityDistribution
 from collections.abc import Generator
 from collections import deque
 from dataclasses import astuple, dataclass
@@ -27,6 +27,7 @@ class PlayerModelNetwork(nn.Module):
         hidden_layer_sizes: list[int] | None = None,
         hidden_layer_activation: type[nn.Module] = nn.LeakyReLU,
         recurrent: nn.RNNBase | bool = False,
+        use_softmax: bool = True,
     ):
         super().__init__()
 
@@ -68,8 +69,8 @@ class PlayerModelNetwork(nn.Module):
             self.layers.append(InputClip(-1, 1, leaky_coef=input_clip_leaky_coef))
 
         self.sigmoid = nn.Sigmoid()
-        self.softmax = nn.Softmax(dim=1)
-        self.log_softmax = nn.LogSoftmax(dim=1)
+        self.prob = nn.Softmax(dim=1) if use_softmax else ProbabilityDistribution(dim=1)
+        self.log_prob = nn.LogSoftmax(dim=1) if use_softmax else LogProbabilityDistribution(dim=1)
 
         # Hidden state if using recurrency
         self._hidden = None
@@ -97,12 +98,12 @@ class PlayerModelNetwork(nn.Module):
     def probabilities(self, obs: T.Tensor, hidden: T.Tensor | None | str = "auto", temperature: float = 1.0) -> tuple[T.Tensor, T.Tensor]:
         """The action probabilities at the given observation."""
         logits, new_hidden = self(obs, hidden)
-        return self.softmax(logits / temperature), new_hidden
+        return self.prob(logits / temperature), new_hidden
 
     def log_probabilities(self, obs: T.Tensor, hidden: T.Tensor | None | str = "auto") -> tuple[T.Tensor, T.Tensor]:
         """The action log-probabilities at the given observation."""
         logits, new_hidden = self(obs, hidden)
-        return self.log_softmax(logits), new_hidden
+        return self.log_prob(logits), new_hidden
 
     def distribution(self, obs: T.Tensor, hidden: T.Tensor | None | str = "auto") -> tuple[T.distributions.Categorical, T.Tensor]:
         """The action distribution at the given observation."""

agents/torch_utils.py
@@ -204,12 +204,23 @@ class InputClip(nn.Module):
 
 
 class ProbabilityDistribution(nn.Module):
-    def __init__(self):
+    def __init__(self, dim: int | None = None):
         """Makes input sum to 1"""
         super().__init__()
+        self.dim = dim
 
     def forward(self, x: T.Tensor):
-        return x / T.sum(x)
+        return x / T.sum(x, dim=self.dim)
+
+
+class LogProbabilityDistribution(nn.Module):
+    def __init__(self, dim: int | None = None):
+        """Log probabilities of a simple probability distribution"""
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x: T.Tensor):
+        return T.log(x / T.sum(x, dim=self.dim))
 
 
 class DebugStoreRecent(nn.Module):